a267e03328053b8f7df2b779eb32d35870f1a559
HUANGsir-JH <huangsirjh2005@gmail.com>
2025-11-09 10:02:27 +0800

    添加冒泡排序

--- code/sample.cpp
+++ code/sample.cpp
@@ -37,6 +37,21 @@ int sort(int arr[], int n) {
     return 0;
 }
 
+// Bubble Sort function to sort an array
+int bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return 0;
+}
+
 int main() {
     int arr[] = {34, 7, 23, 32, 5, 62};
     int n = sizeof(arr) / sizeof(arr[0]);
